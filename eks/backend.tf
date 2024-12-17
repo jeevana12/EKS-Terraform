0986e939b510f618d17ce7ed68ac17d5d8619d6b
jeevana12 <143477580+jeevana12@users.noreply.github.com>
2024-12-17 08:43:31 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,1 +1,17 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "tf-bucket23"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "tf-infra-table"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
